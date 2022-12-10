79bd0a74c87f57ee737c04788ac5c4e3d72efb48
loneranger <58980366+loneranger455-maker@users.noreply.github.com>
2022-12-10 18:23:48 +0545

    enabled debug mode and disconnected from ngix

--- stupact/settings.py
+++ stupact/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--@y1_suj^6o_1^_&az-84o9$2qjpzs43zm^@8!k!@2&vg7s%&7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["stupact.herokuapp.com","0.0.0.0","127.0.0.1"]
 #ALLOWED_HOSTS = []
@@ -117,14 +117,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-
+ 
 STATIC_URL = '/static/'
 STATIC_ROOT=BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 STATICFILES_DIRS = [
-  
+#    os.path.join(BASE_DIR, "static"),
+#     '/var/www/static/',
 ]
 
 
